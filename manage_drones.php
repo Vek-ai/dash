697ff560b978719aa051cc5a05d36d1f071df8ea
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-10-22 10:25:16 +0800

    Removed 3-dots-dropdown-menu

--- manage_drones.php
+++ manage_drones.php
@@ -18,10 +18,6 @@
     <link rel="stylesheet" href="assets/js/dataTable/lib/jquery.dataTables/css/DT_bootstrap.css" />
     <link rel="stylesheet" href="assets/js/dataTable/css/datatables.responsive.css" />
 
-    <!-- 3 Dots Dropdown -->
-    <!-- NOTE: This causes problem to login if transfered to header.php -->
-    <link href="assets\custom\css\3-dots-dropdown-menu.css" rel="stylesheet" type="text/css" />
-
     <!-- Fav and touch icons -->
     <link rel="shortcut icon" href="assets/ico/minus.png">
 
@@ -131,5 +127,3 @@
         <script src="assets/js/footable/js/footable.paginate.js?v=2-0-1" type="text/javascript"></script>
         <script src="assets/js/footable/js/footable.paginate.js?v=2-0-1" type="text/javascript"></script>
 </body>
-
-<script src="assets\custom\js\3-dots-dropdown-menu.js"></script>
